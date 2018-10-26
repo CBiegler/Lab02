d8a4ff4895356655bf1d20ae054464b5d8f29b2e
Jose Montanez <j.eduardo.montanez@gmail.com>
2018-10-26 16:52:54 -0700

    no conflict

--- src/Hello.java
+++ src/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
 		System.out.println("218923485 - This line was added by Kimlee Chea.");
 		System.out.println("219978045 - This line was added by Vu Nguyen.");
 		System.out.println("217309314 - This line was added by Ildebrando Sagerdahl.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("214366153 - This line was added by Jose Montanez.");
 
 	}
 }
