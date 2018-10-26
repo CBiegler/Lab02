22f086feb53a041e10f705105f3b08faa0a1cf4e
Ildebrando Sagerdahl <ildebrandosagerdahl@csus.edu>
2018-10-26 12:41:47 -0700

    added info

--- src/Hello.java
+++ src/Hello.java
@@ -6,8 +6,13 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("219751104 - This line was added Colby Biegler.");
+<<<<<<< master
 		System.out.println("218923485 - This line was added by Kimlee Chea.");
 		System.out.println("219978045 - This line was added by Vu Nguyen.");
+=======
+		System.out.println("217309314 - This line was added by Ildebrando Sagerdahl.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+>>>>>>> added info
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
